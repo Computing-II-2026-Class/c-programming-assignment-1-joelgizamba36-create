c7e8dcd0f631c5be6c867d5da0b55acbb6c92e5a
joelgizamba36-create <joelgizamba36@gmail.com>
2026-03-24 19:31:06 +0300

    Implement electricity bill calculation logic
    Added functionality to calculate electricity bill based on units consumed.

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,34 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: GIZAMBA JOEL */
+/* Student Number: 25/U/BIE/01370/PE */
 
 #include <stdio.h>
+int main(){
+    float units, bill_amount;
+    printf("enter the number of units consumed:");
+    scanf("%f", &units);
 
-int main(void)
-{
-    /* Write your code here */
+    if (units <= 100){
+        bill_amount = 0.5 * units;
+        printf("the bill amount is: %f\n", bill_amount);
+
+    }
+
+    else if (units > 100 && units <= 200){
+        bill_amount = 0.75 * units;
+        printf("the bill amount is: %f\n", bill_amount);
+    }
+
+    else if (units > 200 && units <= 300){
+        bill_amount = 1.20 * units;
+        printf("the bill amount is: %f\n", bill_amount);
+
+    }
+
+    else if (units > 300){
+        bill_amount = 1.50 * units;
+        printf("the bill amount is: %f\n", bill_amount);
+    }
+
 
     return 0;
 }
