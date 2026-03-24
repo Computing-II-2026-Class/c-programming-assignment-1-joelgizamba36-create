008022485906b827a4c238d15c734171ab568a0a
joelgizamba36-create <joelgizamba36@gmail.com>
2026-03-24 20:04:05 +0300

    Add cost per unit to bill calculation

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -3,32 +3,34 @@
 
 #include <stdio.h>
 int main(){
-    float units, bill_amount;
+    float units, bill_amount, cost_per_unit;
     printf("enter the number of units consumed:");
     scanf("%f", &units);
+    printf("enter the cost per unit:");
+    scanf("%f", &cost_per_unit);
 
     if (units <= 100){
-        bill_amount = 0.5 * units;
+        bill_amount = 0.5 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
 
     }
 
     else if (units > 100 && units <= 200){
-        bill_amount = 0.75 * units;
+        bill_amount = 0.75 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
     }
 
     else if (units > 200 && units <= 300){
-        bill_amount = 1.20 * units;
+        bill_amount = 1.20 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
 
     }
 
     else if (units > 300){
-        bill_amount = 1.50 * units;
+        bill_amount = 1.50 * units *cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
     }
-
-
     return 0;
-}
+        
+    }
+
