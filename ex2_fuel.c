4a32f1b48f6a4be6c5a9546940e7aa13609483bb
joelgizamba36-create <joelgizamba36@gmail.com>
2026-03-24 19:33:22 +0300

    Update author information and implement fuel efficiency calculation

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,14 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: GIZAMBA JOEL */
+/* Student Number: 25/U/BIE/01370/PE */
 
 #include <stdio.h>
-
-int main(void)
-{
-    /* Write your code here */
-
+int main(){
+    float distance, fuel_efficiency, fuel_used;
+    printf("Enter the distance to be traveled (in kilometers): ");
+    scanf("%f", &distance);
+    printf("Enter the fuel used of the vehicle (in liters): ");
+    scanf("%f", &fuel_used);
+    fuel_efficiency = distance / fuel_used;
+    printf("The amount of fuel efficiency is: %f km/l\n", fuel_efficiency);
     return 0;
 }
